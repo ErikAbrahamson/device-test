346a3f4a8c1413309d713d4309fc47a93e510461
ErikAbrahamson <e.a.abrahamson@gmail.com>
2016-01-06 14:40:52 -0700

    Finish solution with server-side logic

--- src/server/models/uid.js
+++ src/server/models/uid.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var UniqueID = new Schema({
-    assignment: { type: String }
-});
+var UniqueID = new Schema({ assignment: { type: String } });
 
 module.exports = mongoose.model('UniqueIDs', UniqueID);
