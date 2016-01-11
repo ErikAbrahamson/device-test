05470848bd447315c72fbb358425cf6fe0dbde26
ErikAbrahamson <e.a.abrahamson@gmail.com>
2016-01-10 22:05:11 -0600

    update comments

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -9,6 +9,8 @@ B) After the browser is restarted, when the visitor hits the page again the brow
 C) After the browser clears cookies, the browser is still assigned the same ID
 D) After the browser clears cache, cookies, and all, the browser is still assigned the same ID
 E) Some, or all, of the browsers (chrome, firefox, opera, IE, Safari, etc.) on the device share the same ID
+F) If you got this to work on desktop, get this to also work on mobile
+G) Create a scalable solution
 */
 
 router.get('/', function(req, res, next) {
