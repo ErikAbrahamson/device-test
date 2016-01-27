8a382d48a298e6b649b6990694cd0dd6cb78c7ca
ErikAbrahamson <e.a.abrahamson@gmail.com>
2016-01-27 16:19:10 -0700

    Update post request

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,23 +2,20 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose-q')(require('mongoose'), { spread: true });
 var UniqueID = require('../models/uid.js');
-var crypto = require('crypto');
 
 router.post('/', function(req, res, next) {
 
-    var buildID = crypto.createHash('sha256')
-        .update(req.connection._peername.address)
-        .digest('hex');
+    var crypto = require('crypto'),
+        buildID = crypto.createHash('sha256')
+            .update(req.connection._peername.address)
+            .digest('hex');
 
     UniqueID.findQ(({ 'fingerprint': buildID }))
-
         .then(function(result) {
-            result.forEach(function(id) {
-                console.log(id);
-            });
+
             if (result.length === 0) {
                 new UniqueID({ fingerprint: buildID }).saveQ()
-                    .then(function(data) { res.json(data); })
+                    .then(function(data) { res.json({ 'data': data, 'result': result }); })
                     .catch(function(error) { res.json(error); });
             } else {
                 var options = { new: false }, query = { 'fingerprint': buildID };
