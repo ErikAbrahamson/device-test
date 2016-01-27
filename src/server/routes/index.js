f251c4d32f5b4f4c88f3ce1433053848b41adf32
ErikAbrahamson <e.a.abrahamson@gmail.com>
2016-01-27 16:27:40 -0700

    troubleshoot heroku bug

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,25 +2,25 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose-q')(require('mongoose'), { spread: true });
 var UniqueID = require('../models/uid.js');
+var crypto = require('crypto');
 
 router.post('/', function(req, res, next) {
 
-    var crypto = require('crypto'),
-        buildID = crypto.createHash('sha256')
-            .update(req.connection._peername.address)
-            .digest('hex');
+    var buildID = crypto.createHash('sha256')
+        .update(req.connection._peername.address)
+        .digest('hex');
 
-    UniqueID.findQ(({ 'fingerprint': buildID }))
+    UniqueID.findQ(({ fingerprint: buildID }))
         .then(function(result) {
 
             if (result.length === 0) {
                 new UniqueID({ fingerprint: buildID }).saveQ()
-                    .then(function(data) { res.json({ 'data': data, 'result': result }); })
+                    .then(function(data) { res.json({ 'data': data, 'result': result, 'peer': req.connection._peername.address }); })
                     .catch(function(error) { res.json(error); });
             } else {
-                var options = { new: false }, query = { 'fingerprint': buildID };
+                var options = { new: false }, query = { fingerprint: buildID };
                 UniqueID.findOneAndUpdateQ(query, buildID, options)
-                    .then(function(data) { res.json(data); })
+                    .then(function(data) { res.json({ 'data': data, 'result': result, 'peer': req.connection._peername.address }); })
                     .catch(function(error) { res.json(error); });
             }
         })
